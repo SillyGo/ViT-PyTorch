992fa6eb750ca22da0b5db58afac6c97800be897
SillyGo <arthurmoraisvillar@gmail.com>
2026-09-13 07:56:14 -0300

    feat: full implementation of the vision transformer architecture

--- src/vit_pytorch/models/simple_ViT.py
+++ src/vit_pytorch/models/simple_ViT.py
@@ -89,3 +89,95 @@ class MLP(nn.Module):
         x = self.dropout(x)
 
         return x
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True,
+                p=0., attn_p=0.):
+        super().__init__()
+
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = Attention(
+            dim,
+            n_heads,
+            qkv_bias,
+            attn_p,
+            p
+        )
+
+        self.norm2 = nn.LayerNorm(dim, 1e-6)
+
+        mlp_hidden_size = int(3 * dim) # caso essa multiplicação n dê um int, force um
+
+        self.mlp = MLP(
+            dim,
+            mlp_hidden_size,
+            dim,
+            p
+        )
+
+    def forward(self, x):
+        x = x + self.attn(self.nomr1(x))    # esse '+x' é devido às conexões residuais no paper.
+        x = x + self.mlp(self.norm2(x))
+
+        return x
+    
+class VisionTransformer(nn.Module):
+    def __init__(self, img_size, patch_size, canais, n_classes, dim, depth,
+                 n_heads, mlp_ratio, qkv_bias, p, attn_p):
+        super().__init__()
+
+        self.patch_embed = PathEmbed(
+            img_size,
+            patch_size,
+            canais,
+            dim
+        )
+
+        self.cls_token = nn.Parameter(torch.zeros(1,1,dim)) # inicializamos o class token com zeros
+        self.pos_embed = nn.Parameter(                      # embeddings the posição. Temos um para cada patch. Esse embedding, obviamente, tem dimensão 'dim'.
+            torch.zeros(1,1,self.patch_embed.n_patches, dim) 
+        )
+
+        self.pos_drop = nn.Dropout(p=p)
+
+        self.blocks = nn.ModuleList(    # cria vários blocos de transformer. Cada um vai ter sua própria quantidade de parâmetros aprendíveis
+            [
+                TransformerBlock(
+                    dim,
+                    n_heads,
+                    mlp_ratio,
+                    qkv_bias,
+                    p,
+                    attn_p
+                )
+                for _ in range(depth)
+            ]
+        )
+
+        self.norm = nn.LayerNorm(dim, eps=1e-6)
+        self.head = nn.Linear(dim, n_classes)
+
+    def forward(self, x):
+
+        # X: imagem. F: torch.Tensor(n_samples, n_channels, img_size, img_size)
+        # essa função deve retornar logits. 
+
+        n_samples = x.shape[0]
+        
+        x = self.patch_embed(x) # gera patch embeddings
+        cls_token = self.cls_token.expand(
+            n_samples, -1, -1
+        )
+
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for attn_block in self.blocks:
+            x = attn_block(x)
+
+        x = self.norm(x)
+
+        cls_token_logit = x[:,0]
+        x = self.head(cls_token_logit)
+
+        return x
